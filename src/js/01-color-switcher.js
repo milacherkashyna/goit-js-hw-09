3bf761907ec78bcb9b7f1729687bfe5384962a78
milacherkashyna <mila.cherkashyna@gmail.com>
2023-02-22 15:03:06 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const startBtnRef = document.querySelector('[data-start]');
+const stopBtnRef = document.querySelector('[data-stop]');
+let timerId = null;
+
+stopBtnRef.disabled = true;
+
+startBtnRef.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  stopBtnRef.disabled = false;
+  startBtnRef.disabled = true;
+});
+
+stopBtnRef.addEventListener('click', () => {
+  clearInterval(timerId);
+  console.log(`Interval with id ${timerId} has stopped!`);
+  stopBtnRef.disabled = true;
+  startBtnRef.disabled = false;
+});
