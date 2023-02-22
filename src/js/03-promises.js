a68725435cc45cf1e5a7b9d5843ac8423a25d923
milacherkashyna <mila.cherkashyna@gmail.com>
2023-02-22 17:13:50 +0200

    homework for checking

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,45 @@
+import Notiflix from 'notiflix';
+const formRef = document.querySelector('.form');
+formRef.addEventListener('submit', onFormSubmit);
+function onFormSubmit(event) {
+  event.preventDefault();
+  if (
+    event.target.elements.delay.value === '' ||
+    event.target.elements.step.value === '' ||
+    event.target.elements.amount.value === ''
+  ) {
+    Notiflix.Notify.failure('Please fill all fields');
+    return;
+  }
+  const amount = +event.target.elements.amount.value;
+  let delay = +event.target.elements.delay.value;
+  const step = +event.target.elements.step.value;
+  event.target.reset();
+  for (let index = 1; index <= amount; index++) {
+    createPromise(index, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+    delay += step;
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
